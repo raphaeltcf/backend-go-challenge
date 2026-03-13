6382fc7d41919afcaa30b8e7aaa27c05a8429791
raphaeltcf <raphaeltcferreira@gmail.com>
2026-03-13 08:57:39 -0300

    feat(infra): configure structured JSON logger with slog

---
+++ internal/infra/logger/logger.go
@@ -0,0 +1,13 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+func NewLogger() *slog.Logger {
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})
+	return slog.New(handler)
+}
